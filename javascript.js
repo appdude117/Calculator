cae394f8af257618991ab338faf1fd990f90c616
appdude117 <browne.liam@outlook.com>
2025-06-17 18:06:33 +1000

    calulation

---
+++ javascript.js
@@ -0,0 +1,58 @@
+const homepage = document.querySelector('#homepage') ;
+const bottom = document.querySelector('#bottom') ;
+
+const One = document.createElement('One');
+One.classList.add("button");
+homepage.appendChild(One);
+
+const Two = document.createElement('Two');
+Two.classList.add("button");
+homepage.appendChild(Two);
+
+const Three = document.createElement('Three');
+Three.classList.add("button");
+homepage.appendChild(Three);
+
+const Four = document.createElement('Four');
+Four.classList.add("button");
+homepage.appendChild(Four);
+
+const Five = document.createElement('Five');
+Five.classList.add("button");
+homepage.appendChild(Five);
+
+const Six = document.createElement('Six');
+Six.classList.add("button");
+homepage.appendChild(Six);
+
+const Seven = document.createElement('Seven');
+Seven.classList.add("button");
+homepage.appendChild(Seven);
+
+const Eight = document.createElement('Eight');
+Eight.classList.add("button");
+homepage.appendChild(Eight);
+
+const Nine = document.createElement('Nine');
+Nine.classList.add("button");
+homepage.appendChild(Nine);
+
+const Zero = document.createElement('Zero');
+Zero.classList.add("button");
+homepage.appendChild(Zero);
+
+const Multiply = document.createElement('Multiply');
+Multiply.classList.add("button");
+bottom.appendChild(Multiply);
+
+const Divide = document.createElement('Divide');
+Divide.classList.add("button");
+bottom.appendChild(Divide);
+
+const Add = document.createElement('Add');
+Add.classList.add("button");
+bottom.appendChild(Add);
+
+const Subtract = document.createElement('Subtract');
+Subtract.classList.add("button");
+bottom.appendChild(Subtract);
