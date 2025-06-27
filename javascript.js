05e91048419eb7357ee73bb3b6f1e132c5992641
appdude117 <browne.liam@outlook.com>
2025-06-27 19:11:16 +1000

    looks ok

--- javascript.js
+++ javascript.js
@@ -3,56 +3,81 @@ const bottom = document.querySelector('#bottom') ;
 
 const One = document.createElement('One');
 One.classList.add("button");
+One.innerHTML = "1" ;
 homepage.appendChild(One);
 
 const Two = document.createElement('Two');
 Two.classList.add("button");
+Two.innerHTML = "2" ;
 homepage.appendChild(Two);
 
 const Three = document.createElement('Three');
 Three.classList.add("button");
+Three.innerHTML = "3" ;
 homepage.appendChild(Three);
 
 const Four = document.createElement('Four');
 Four.classList.add("button");
+Four.innerHTML = "4" ;
 homepage.appendChild(Four);
 
 const Five = document.createElement('Five');
 Five.classList.add("button");
+Five.innerHTML = "5" ;
 homepage.appendChild(Five);
 
 const Six = document.createElement('Six');
 Six.classList.add("button");
+Six.innerHTML = "6" ;
 homepage.appendChild(Six);
 
 const Seven = document.createElement('Seven');
 Seven.classList.add("button");
+Seven.innerHTML = "7" ;
 homepage.appendChild(Seven);
 
 const Eight = document.createElement('Eight');
 Eight.classList.add("button");
+Eight.innerHTML = "8" ;
 homepage.appendChild(Eight);
 
 const Nine = document.createElement('Nine');
 Nine.classList.add("button");
+Nine.innerHTML = "9" ;
 homepage.appendChild(Nine);
 
 const Zero = document.createElement('Zero');
 Zero.classList.add("button");
+Zero.innerHTML = "0" ;
 homepage.appendChild(Zero);
 
 const Multiply = document.createElement('Multiply');
 Multiply.classList.add("button");
+Multiply.innerHTML = "*" ;
 bottom.appendChild(Multiply);
 
 const Divide = document.createElement('Divide');
 Divide.classList.add("button");
+Divide.innerHTML = "/" ;
 bottom.appendChild(Divide);
 
 const Add = document.createElement('Add');
 Add.classList.add("button");
+Add.innerHTML = "+" ;
 bottom.appendChild(Add);
 
 const Subtract = document.createElement('Subtract');
 Subtract.classList.add("button");
+Subtract.innerHTML = "-" ;
 bottom.appendChild(Subtract);
+
+const Equals = document.createElement('Equals');
+Equals.classList.add("button");
+Equals.innerHTML = "=" ;
+bottom.appendChild(Equals);
+
+const Clear = document.createElement('Clear');
+Clear.classList.add("button");
+Clear.innerHTML = "C" ;
+bottom.appendChild(Clear);
+
